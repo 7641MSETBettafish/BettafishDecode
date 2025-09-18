3c31c29a002ed03f3bfa0de9883c92289b10ecd2
Absolute Symmetry <laum9544@lgsstudent.org>
2025-09-17 20:45:54 -0700

    kS and kV
    I fixed forward ramp logger and got values for kS and kV.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -68,8 +68,8 @@ public final class MecanumDrive {
         public double trackWidthTicks = 0;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
+        public double kS = 0.907931278434138;
+        public double kV = 0.0002714532551083228;
         public double kA = 0;
 
         // path profile parameters (in inches)
