9e5f3c0ed863293158e951e40335518c0256ca39
masonc8419 <masonc3000@gmail.com>
2025-09-21 18:45:35 -0700

    Create Example.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/practice/Example.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.practice;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+//ur reference opmode template
+
+@TeleOp
+public class Example extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            
+        }
+
+    }
+
+}
