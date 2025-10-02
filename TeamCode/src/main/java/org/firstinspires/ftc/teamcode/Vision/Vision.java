4fd20074a6e1f5fe24502f099d0d559f8e1dcf2e
masonc8419 <masonc3000@gmail.com>
2025-10-01 22:13:50 -0700

    camera math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/Vision.java
@@ -2,10 +2,14 @@ package org.firstinspires.ftc.teamcode.Vision;
 
 import android.util.Size;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -20,6 +24,10 @@ public class Vision extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        Pose2d startPose = new Pose2d(57, 60.5, Math.toRadians(0));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
 
         tagProcessor = new AprilTagProcessor.Builder()
                 .setDrawAxes(true)
@@ -34,33 +42,39 @@ public class Vision extends LinearOpMode {
                 .setCameraResolution(new Size(640, 480))
                 .build();
 
+        // inches
+        double z = 29.5 - 18.5;
+        double x;
+        double y;
+
         waitForStart();
 
         while (!isStopRequested() && opModeIsActive()) {
             AprilTagDetection tag;
             if (!tagProcessor.getDetections().isEmpty()) {
                 tag = tagProcessor.getDetections().get(0);
+
                 if(tag.metadata != null) {
+
+                    x = z * Math.sin(Math.toRadians(tag.ftcPose.bearing + 180 - Math.toDegrees(drive.localizer.getPose().heading.toDouble()))) / Math.tan(Math.toRadians(tag.ftcPose.elevation));
+                    y = z * Math.cos(Math.toRadians(tag.ftcPose.bearing + 180 - Math.toDegrees(drive.localizer.getPose().heading.toDouble()))) / Math.tan(Math.toRadians(tag.ftcPose.elevation));
+
                     telemetry.addData("id", tag.metadata.id);
-                    telemetry.addData("x", tag.ftcPose.x);
-                    telemetry.addData("y", tag.ftcPose.y);
-                    telemetry.addData("z", tag.ftcPose.z);
                     telemetry.addData("roll", tag.ftcPose.roll);
                     telemetry.addData("pitch", tag.ftcPose.pitch);
                     telemetry.addData("yaw", tag.ftcPose.yaw);
+                    telemetry.addData("bearing", tag.ftcPose.bearing);
+                    telemetry.addData("elevation", tag.ftcPose.elevation);
+                    telemetry.addData("Expected X", x);
+                    telemetry.addData("Expected Y", y);
                 }
-
             }
-
+            telemetry.addData("robot X", drive.localizer.getPose().position.x);
+            telemetry.addData("robot Y", drive.localizer.getPose().position.y);
+            telemetry.addData("robot H", Math.toDegrees(drive.localizer.getPose().heading.toDouble()));
+            drive.updatePoseEstimate();
             telemetry.update();
         }
-    }
 
-    public int findID() {
-        List<AprilTagDetection> detections = tagProcessor.getDetections();
-        if (detections != null && !detections.isEmpty()) {
-            return detections.get(0).metadata.id;
-        }
-        return -1;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TransfertoShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TransfertoShooterTest.java
@@ -5,28 +5,29 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.teamcode.mechanisms.Intake;
 
 @Config
 @TeleOp(name="IntakeTest2", group="Testing")
-public class IntakeTest2 extends LinearOpMode {
+public class TransfertoShooterTest extends LinearOpMode {
 
     public static double motorPower = 0.5;
+    public static double shootermotorPower = 0.5;
     public static int detectionDistance = 5;
 
     DcMotor intakeMotor;
     DcMotor transferMotor;
+    DcMotor leftShooterMotor;
+    DcMotor rightShooterMotor;
 
     public DistanceSensor transferSensor;
 
     @Override
     public void runOpMode() {
 
-        intakeMotor = hardwareMap.get(DcMotor.class, "intakeShooter");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         transferMotor = hardwareMap.get(DcMotor.class, "transferMotor");
+        leftShooterMotor = hardwareMap.get(DcMotor.class, "leftShooter");
+        rightShooterMotor = hardwareMap.get(DcMotor.class, "rightShooter");
         //transferSensor = hardwareMap.get(DistanceSensor.class, "transferSensor");
 
         waitForStart();
@@ -35,6 +36,9 @@ public class IntakeTest2 extends LinearOpMode {
         while (opModeIsActive()) {
             transferMotor.setPower(-motorPower);
             intakeMotor.setPower(motorPower);
+
+            leftShooterMotor.setPower(shootermotorPower);
+            rightShooterMotor.setPower(-shootermotorPower);
 
             /*if (transferSensor.getDistance(DistanceUnit.CM) <= detectionDistance) {
                 transferMotor.setPower(0);
