01a3c81cc6cb0980920823acb028faf323a65721
masonc8419 <masonc3000@gmail.com>
2025-10-03 21:47:22 -0700

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TransfertoShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TransfertoShooterTest.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -29,24 +30,33 @@ public class TransfertoShooterTest extends LinearOpMode {
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
         transferMotor = hardwareMap.get(DcMotor.class, "transferMotor");
         leftShooterMotor = hardwareMap.get(DcMotor.class, "leftShooter");
-        rightShooterMotor = hardwareMap.get(DcMotor.class, "rightShooter"   );
+        rightShooterMotor = hardwareMap.get(DcMotor.class, "rightShooter");
         transferSensor = hardwareMap.get(DistanceSensor.class, "transferSensor");
+        ElapsedTime time = new ElapsedTime();
+        double shootTime = 0;
 
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-
-            leftShooterMotor.setPower(shootermotorPower);
-            rightShooterMotor.setPower(-shootermotorPower);
-
-            while (transferSensor.getDistance(DistanceUnit.CM) >= detectionDistance) {
-                intakeMotor.setPower(motorPower);
-                transferMotor.setPower(-motorPower);
-                telemetry.addData("distancesensor", transferSensor.getDistance(DistanceUnit.CM));
-                telemetry.update();
+            time.reset();
+            if (gamepad1.a) {
+                while (transferSensor.getDistance(DistanceUnit.CM) >= detectionDistance) {
+                    intakeMotor.setPower(motorPower);
+                    transferMotor.setPower(-motorPower);
+                }
+                intakeMotor.setPower(0);
             }
-            intakeMotor.setPower(0);
+            if (gamepad1.b) {
+                leftShooterMotor.setPower(shootermotorPower);
+                rightShooterMotor.setPower(-shootermotorPower);
+                shootTime = 0;
+            }
+            if (gamepad1.y && shootTime >= 1000) {
+                intakeMotor.setPower(motorPower);
+            }
+
+            shootTime += time.milliseconds();
 
 
         }
