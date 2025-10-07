4ff45eb2afd33f09472d8ed4f3e372cd7d9582a0
masonc8419 <masonc3000@gmail.com>
2025-10-06 21:16:46 -0700

    rpm cooked

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/practiceh/Example.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/practiceh/Example.java
@@ -1,22 +1,22 @@
-package org.firstinspires.ftc.robotcontroller.practiceh;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-//ur reference opmode template
-
-@TeleOp
-public class Example extends LinearOpMode {
-
-    @Override
-    public void runOpMode() {
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-
-        }
-
-    }
-
-}
+//package org.firstinspires.ftc.robotcontroller.practiceh;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//
+////ur reference opmode template
+//
+//@TeleOp
+//public class Example extends LinearOpMode {
+//
+//    @Override
+//    public void runOpMode() {
+//
+//        waitForStart();
+//
+//        while (opModeIsActive()) {
+//
+//        }
+//
+//    }
+//
+//}
