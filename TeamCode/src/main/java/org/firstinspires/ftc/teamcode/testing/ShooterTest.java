8fd5d4ff09661c3f564d5126eea655ad1ac97380
Hansika1098 <hansika.varanasi@gmail.com>
2025-10-12 16:59:26 -0700

    hey

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/ShooterTest.java
@@ -37,12 +37,12 @@ public class ShooterTest extends LinearOpMode {
         while (opModeIsActive()) {
 
             shooter.updateRPM();
-            Shooter.targetRPM = motorPower * Shooter.MOTOR_NO_LOAD_RPM * Shooter.GEAR_RATIO;
+            shooter.targetRPM = motorPower * Shooter.MOTOR_NO_LOAD_RPM * Shooter.GEAR_RATIO;
             shooter.updatePID();
 
             telemetry.addData("leftRPM", shooter.leftRPM);
             telemetry.addData("rightRPM", shooter.rightRPM);
-            telemetry.addData("targetRPM", Shooter.targetRPM);
+            telemetry.addData("targetRPM", shooter.targetRPM);
             telemetry.addData("leftPower", shooter.leftShooterMotor.getPower());
             telemetry.addData("rightPower", shooter.rightShooterMotor.getPower());
             telemetry.addData("targetPower", motorPower);
